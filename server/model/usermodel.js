d7f04d167b2d3ecda7c20ec15fec00751ace4dd6
osesoguogho <osesthedon@gmail.com>
2025-11-18 21:10:13 +0100

    timestamps add

--- server/model/usermodel.js
+++ server/model/usermodel.js
@@ -5,7 +5,8 @@ const userSchema = new mongoose.Schema({
     email: {type: String, required: true, unique: true},
     password: {type: String, reuired: true},
     createdAt: {type: Date, default: new Date()}
-}
+},
+{timestamps : true}
 );
 
 const users = mongoose.model("users", userSchema);
@@ -18,15 +19,17 @@ const expenseSchema = new mongoose.Schema({
     payment_method: {type: String, require: true},
     amount: {type: Number, required: true, default: 0},
     // purseBalance: {type: mongoose.SchemaTypes.ObjectId, ref: "purseMoney"},
-    createdAt: {type: Date, default: new Date().toLocaleString()}
-});
+    // createdAt: {type: Date, default: new Date().toLocaleString()}
+},
+{timestamps : true});
 
 const expenses = mongoose.model("expenses", expenseSchema);
 
 const purse = new mongoose.Schema({
     email: {type: String, require: true},
     purse_balance: {type: Number, require: true}
-});
+},
+{timestamps : true});
 
 const purseMoney = mongoose.model("purseMoney", purse);
 
